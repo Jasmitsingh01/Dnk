488d701c5231eb308a6db8b10a94fe768bdde370
Jasmit Singh <97171634+Jasmitsingh01@users.noreply.github.com>
2024-02-19 20:42:24 +0530

    Update Banner.jsx

--- src/components/static/Banner.jsx
+++ src/components/static/Banner.jsx
@@ -1,7 +1,7 @@
 
 function Banner() {
   return (
-<section className=" relative  w-full bg-center bg-banner-1 bg-cover px-2 min-h-[80vh] lg:min-h-[100vh] before:absolute before:h-full before:w-full before:top-0 before:-left-0 before:bg-banner-overlay before:z-10 z-0  flex  items-center  lg:bg-fixed lg:w-full " >
+<section className=" relative  w-full bg-center bg-banner-1 bg-cover px-2 min-h-[80vh] lg:min-h-[100vh] before:absolute before:h-full before:w-full before:top-0 before:-left-0 before:bg-banner-overlay before:z-10 z-0  flex  items-center  lg:bg-fixed lg:w-full " style={{backgroundImage :'url("../../banner-1.jpg")'}} >
         <div className="text-center md:text-left z-20 mx-auto md:mx-20">
           <h1 className="text-4xl w-full min-[545px]:w-[500px] min-[545px]:text-5xl md:text-8xl md:w-[600px] lg:w-[800px] md:leading-normal	md:mb-1	 text-white font-semibold mb-4 line ">
             Raining Offers For Hot Summer!
