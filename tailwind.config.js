c1547b648819f8b3cada4032179921d9139b3cb7
Jasmitsingh01 <jasmits007@gmail.com>
2024-02-19 20:27:37 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: {  
+      
+      colors:{
+        'nav':"#403f3f4f",
+        'banner-overlay':"#0b4463ad"
+      }
+    },
+    backgroundImage:{  
         'banner-1':"url('./src/assets/banner_1.jpg')"  ,
         'card-1':"url('./src/assets/women_card.jpg')",
         'card-2':"url('./src/assets/men_card.jpg')",
@@ -14,14 +20,8 @@ export default {
         'banner-2':"url('/src/assets/banner-03.jpg')",
         'banner-3':"url('/src/assets/banner-04.jpg')",
         'banner-4':"url('/src/assets/banner-05.jpg')",
-        'banner-5':"url('/src/assets/banner-06.jpg')",
-
-        
-      },
-      colors:{
-        'nav':"#403f3f4f",
-        'banner-overlay':"#0b4463ad"
-      }
+        'banner-5':"url('/src/assets/banner-06.jpg')",    
+      
     }
     
   },
